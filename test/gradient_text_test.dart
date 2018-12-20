9775fb0e192388b4fb5053c2f4fd70ba8854cacd
Anthony Owen <tony@owentech.co.uk>
2018-12-20 17:36:24 +0000

    Initial commit

---
+++ test/gradient_text_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gradient_text/gradient_text.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  test('font size is carried to text widget', () {
+    final gradientText = GradientText("this is a test", gradient: LinearGradient(colors: [Colors.red, Colors.black]), style: TextStyle(fontSize: 42),);
+    expect(gradientText.style.fontSize, 42);
+  });
+}
